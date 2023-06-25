0534dbce7bee80d43a078f5d46fb57a4ccaf9eea
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-06-25 14:18:15 -0500

    changed list items to buttons, changed title of page, added a start button

--- assets/script.js
+++ assets/script.js
@@ -4,38 +4,39 @@ var choice2 = document.getElementById("2");
 var choice3 = document.getElementById("3");
 var choice4 = document.getElementById("4");
 
+// These are placeholder questions
 var questions = ["What is Sherry's name?", "How old is Sherry?", "What is Sherry's cat's name?", "What planet are we on?"];
 
 var answers = [
     {
-        choice1: "Donna",
-        choice2: "Sherry",
-        choice3: "Catherine",
-        choice4: "Meghan",
+        choice1: "1. Donna",
+        choice2: "2. Sherry",
+        choice3: "3. Catherine",
+        choice4: "4. Meghan",
         answer: 2,
     },
 
     {
-        choice1: "2",
-        choice2: "12",
-        choice3: "150",
-        choice4: "37",
+        choice1: "1. 2",
+        choice2: "2. 12",
+        choice3: "3. 150",
+        choice4: "4. 37",
         answer: 4,
     },
 
     {
-        choice1: "Fluffy",
-        choice2: "Bozo",
-        choice3: "Pacino",
-        choice4: "Cat",
+        choice1: "1. Fluffy",
+        choice2: "2. Bozo",
+        choice3: "3. Pacino",
+        choice4: "4. Cat",
         answer: 3,
     },
 
     {
-        choice1: "Earth",
-        choice2: "Mars",
-        choice3: "Venus",
-        choice4: "Pluto",
+        choice1: "1. Earth",
+        choice2: "2. Mars",
+        choice3: "3. Venus",
+        choice4: "4. Pluto",
         answer: 1,
     },
 ]
@@ -48,3 +49,11 @@ for (let index = 0; index < questions.length; index++) {
     choice4.innerText = answers[index].choice4;
     
 }
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert(button.innerText);
+    });
+})
