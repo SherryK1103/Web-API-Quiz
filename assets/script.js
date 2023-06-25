b5ad708b46638e549e6cdb045e49295d2c902b9e
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-06-25 18:26:26 -0500

    added for loop inside displayquest function

--- assets/script.js
+++ assets/script.js
@@ -72,7 +72,13 @@ var answers = [
 
 
 function displayQuest () {
-    console.log('running displayQuest') 
+    for (let index = 0; index < questions.length; index++) {
+        paraQuest.innerText = questions[index];
+        choice1.innerText = answers[index].choice1;
+        choice2.innerText = answers[index].choice2;
+        choice3.innerText = answers[index].choice3;
+        choice4.innerText = answers[index].choice4;
+     }
 paraQuest.innerText = answers[index].question;
 choice1.innerText = answers[index].choice1;
 choice2.innerText = answers[index].choice2;
