6a18d31aa51bb3d35ddea2988f3ddc4a62dde244
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-06-26 11:04:32 -0500

    removed for loop, added event listeners to buttons, created if statements

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ var choice3 = document.getElementById("3");
 var choice4 = document.getElementById("4");
 var startButton = document.getElementById("startButton");
 var index = 0;
+var totalTime = 600;
 
 
 // These are placeholder questions
@@ -17,7 +18,7 @@ var answers = [
         choice2: "2. Sherry",
         choice3: "3. Catherine",
         choice4: "4. Meghan",
-        answer: 2,
+        correctAnswer: 2,
     },
 
     {
@@ -26,7 +27,7 @@ var answers = [
         choice2: "2. 12",
         choice3: "3. 150",
         choice4: "4. 37",
-        answer: 4,
+        correctAnswer: 4,
     },
 
     {
@@ -35,7 +36,7 @@ var answers = [
         choice2: "2. Bozo",
         choice3: "3. Pacino",
         choice4: "4. Cat",
-        answer: 3,
+        correctAnswer: 3,
     },
 
     {
@@ -44,9 +45,58 @@ var answers = [
         choice2: "2. Mars",
         choice3: "3. Venus",
         choice4: "4. Pluto",
-        answer: 1,
+        correctAnswer: 1,
     },
 ]
+
+choice1.addEventListener('click', () => {
+    if (answers[index].correctAnswer == 1) {
+        index += 1;
+        displayQuest();
+    }
+    else {
+       totalTime -= 3;
+    }
+ }); 
+
+choice2.addEventListener('click', () => {
+    if (answers[index].correctAnswer == 2) {
+        index += 1;
+        displayQuest();
+    }
+    else {
+       totalTime -= 3;
+    }
+ });
+
+ choice3.addEventListener('click', () => {
+    if (answers[index].correctAnswer == 3) {
+        index += 1;
+        displayQuest();
+    }
+    else {
+       totalTime -= 3;
+    }
+ });
+
+ choice4.addEventListener('click', () => {
+    if (answers[index].correctAnswer == 4) {
+        index += 1;
+        displayQuest();
+    }
+    else {
+       totalTime -= 3;
+    }
+ });
+
+function timer() {
+    totalTime -= 1;
+}
+
+setInterval(timer, 1000);
+
+ // display the timer and make it countdown. We need to do set interval 
+ // update event listeners to proceed even if answer is wrong, just proceed with even less time. 
 
 // for (let index = 0; index < questions.length; index++) {
    // paraQuest.innerText = questions[index];
@@ -72,13 +122,6 @@ var answers = [
 
 
 function displayQuest () {
-    for (let index = 0; index < questions.length; index++) {
-        paraQuest.innerText = questions[index];
-        choice1.innerText = answers[index].choice1;
-        choice2.innerText = answers[index].choice2;
-        choice3.innerText = answers[index].choice3;
-        choice4.innerText = answers[index].choice4;
-     }
 paraQuest.innerText = answers[index].question;
 choice1.innerText = answers[index].choice1;
 choice2.innerText = answers[index].choice2;
@@ -89,3 +132,26 @@ choice4.innerText = answers[index].choice4;
 // displayQuest()
 
 startButton.addEventListener("click", displayQuest)
+
+/* choice1.addEventListener('click', () => {
+    if (answers[index].answer != 1) {
+        totalTime -= 3;
+     }
+ 
+     index += 1;
+       displayQuest();
+  }); 
+
+setInterval(myTimer, 1000);
+
+
+setInterval(timer, 1000);
+
+
+
+if (answers[index].answer == 2) {
+         alert("correct")
+     }
+    else {
+       alert("incorrect")
+     } */
