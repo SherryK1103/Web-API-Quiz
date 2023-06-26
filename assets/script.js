505d50a3c90735a98aa78e4ed701c5310da4a58b
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-06-26 16:10:10 -0500

    added score ID to keep track of score and added variables to display the score on the webpage

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,8 @@ var startButton = document.getElementById("startButton");
 var totalTime = 60;
 var timeDisplay = document.getElementById("time");
 var score = 0; // Variable to store the user's score
+var scorerDisplay = document.getElementById("high-scores");
+var scoreDisplay = document.getElementById("score");
 
 // These are placeholder questions
 var questions = ["What does HTML stand for?", "What does CSS stand for?", "What is Sherry's cat's name?", "What planet are we on?"];
@@ -24,10 +26,10 @@ var answers = [
 
     {
         question: "What does CSS stand for?",
-        choice1: "1. 2",
-        choice2: "2. 12",
-        choice3: "3. 150",
-        choice4: "4. 37",
+        choice1: "1. Cisco Silly Sass",
+        choice2: "2. Caring Sharing Sharing",
+        choice3: "3. Cool Sisters Soul",
+        choice4: "4. Cascading Style Sheet",
         correctAnswer: 4,
     },
 
@@ -51,44 +53,48 @@ var answers = [
 ]
 
 choice1.addEventListener('click', () => {
-    if (answers[index].correctAnswer == 1) {
-        score +=1; // increment the score if the answer is correct
+    if (answers[index].correctAnswer === 1) {
+        score += 1; // increment the score if the answer is correct
     } else {
-       totalTime -= 3;
+        totalTime -= 3;
     }
-       index += 1;
-       displayQuest();
- }); 
+    index += 1;
+    updateScoreDisplay();
+    displayQuest();
+});
 
 choice2.addEventListener('click', () => {
-    if (answers[index].correctAnswer == 1) {
-        score +=1; // increment the score if the answer is correct
+    if (answers[index].correctAnswer === 2) {
+        score += 1;
     } else {
-       totalTime -= 3;
+        totalTime -= 3;
     }
-       index += 1;
-       displayQuest();
- });
+    index += 1;
+    updateScoreDisplay();
+    displayQuest();
+});
 
- choice3.addEventListener('click', () => {
-    if (answers[index].correctAnswer == 1) {
-        score +=1; // increment the score if the answer is correct
+choice3.addEventListener('click', () => {
+    if (answers[index].correctAnswer === 3) {
+        score += 1;
     } else {
-       totalTime -= 3;
+        totalTime -= 3;
     }
-       index += 1;
-       displayQuest();
- });
+    index += 1;
+    updateScoreDisplay();
+    displayQuest();
+});
 
- choice4.addEventListener('click', () => {
-    if (answers[index].correctAnswer == 1) {
-        score +=1; // increment the score if the answer is correct
+choice4.addEventListener('click', () => {
+    if (answers[index].correctAnswer === 4) {
+        score += 1;
     } else {
-       totalTime -= 3;
+        totalTime -= 3;
     }
-       index += 1;
-       displayQuest();
- });
+    index += 1;
+    updateScoreDisplay();
+    displayQuest();
+});
 
 function timer() {
     totalTime -= 1;
@@ -102,42 +108,43 @@ var timerInterval; // variable to store the interval ID
 
 setInterval(timer, 1000);
 
- // display the timer and make it countdown. We need to do set interval 
- // update event listeners to proceed even if answer is wrong, just proceed with even less time. 
-
 function displayQuest() {
-paraQuest.innerText = answers[index].question;
-choice1.innerText = answers[index].choice1;
-choice2.innerText = answers[index].choice2;
-choice3.innerText = answers[index].choice3;
-choice4.innerText = answers[index].choice4;
-if (index >= answers.length) {
-    endQuiz();
-    return;
-}
+    paraQuest.innerText = answers[index].question;
+    choice1.innerText = answers[index].choice1;
+    choice2.innerText = answers[index].choice2;
+    choice3.innerText = answers[index].choice3;
+    choice4.innerText = answers[index].choice4;
+    if (index >= answers.length) {
+        endQuiz();
+        return;
+    }
 }
 
 function endQuiz() {
     // Stop the timer
     clearInterval(timerInterval);
 
-    // Disable answer choices ot perform any other actions
+    // Disable answer choices to perform any other actions
     choice1.disabled = true;
     choice2.disabled = true;
     choice3.disabled = true;
     choice4.disabled = true;
 
-    // Display a message to indicate end of quiz
+    // Display a message to indicate the end of the quiz
     paraQuest.textContent = "Quiz ended!";
     timeDisplay.textContent = "Time's up!";
 }
 
 function startTimer() {
-        totalTime--;
-        timeDisplay.textContent = "Time Remaining: " + totalTime;
-        if (totalTime <= 0) {
-            endQuiz();
-        }
+    totalTime--;
+    timeDisplay.textContent = "Time Remaining: " + totalTime;
+    if (totalTime <= 0) {
+        endQuiz();
+    }
+}
+
+function updateScoreDisplay() {
+    scoreDisplay.textContent = "Score: " + score;
 }
 
 startButton.addEventListener("click", function() {
